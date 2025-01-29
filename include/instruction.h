21c58ae06d3d8e3ec866a78ba32bd10bbf2ff4d5
sheasewall <sheasewall@gmail.com>
2025-01-29 16:46:26 -0500

    Added RISC-V instruction types

--- include/instruction.h
+++ include/instruction.h
@@ -2,12 +2,14 @@
 #define INSTRUCTION_H
 
 #include "fileiounit.hpp"
+#include "register.h"
 
 #include <sstream>
 #include <unordered_map>
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <bitset>
 
 using Reg = Register<int>*;
 
@@ -48,6 +50,161 @@ struct Instruction
     virtual ~Instruction() {}
     virtual void execute() = 0;
 };
+
+/// RISC-V Types
+
+// Register-Register operation
+struct RType : Instruction 
+{
+    std::bitset<7> opcode;
+    std::bitset<5> rd;
+    std::bitset<3> funct3;
+    std::bitset<5> rs1;
+    std::bitset<5> rs2;
+    std::bitset<7> funct7;
+
+    RType(std::bitset<32> instruction)
+    {
+        std::string instr = instruction.to_string();
+
+        funct7 = std::bitset<7>(instr.substr(0, 7));
+        rs2 = std::bitset<5>(instr.substr(7, 5));
+        rs1 = std::bitset<5>(instr.substr(12, 5));
+        funct3 = std::bitset<3>(instr.substr(17, 3));
+        rd = std::bitset<5>(instr.substr(20, 5));
+        opcode = std::bitset<7>(instr.substr(25, 7));
+    }
+
+    void execute() override {}
+};
+
+// Immediate operations
+struct IType : Instruction 
+{
+    std::bitset<7> opcode;
+    std::bitset<5> rd;
+    std::bitset<3> funct3;
+    std::bitset<5> rs1;
+    std::bitset<12> imm;
+
+    IType(std::bitset<32> instruction)
+    {
+        std::string instr = instruction.to_string();
+
+        imm = std::bitset<12>(instr.substr(0, 12));
+        rs1 = std::bitset<5>(instr.substr(12, 5));
+        funct3 = std::bitset<3>(instr.substr(17, 3));
+        rd = std::bitset<5>(instr.substr(20, 5));
+        opcode = std::bitset<7>(instr.substr(25, 7));
+    }
+
+    void execute() override {}
+};
+
+// Store operations
+struct SType : Instruction 
+{
+    std::bitset<7> opcode;
+    std::bitset<3> funct3;
+    std::bitset<5> rs1;
+    std::bitset<5> rs2;
+    std::bitset<12> imm;
+
+    SType(std::bitset<32> instruction)
+    {
+        std::string instr = instruction.to_string();
+
+        std::bitset<7> imm1 = std::bitset<7>(instr.substr(0, 7));
+        rs2 = std::bitset<5>(instr.substr(7, 5));
+        rs1 = std::bitset<5>(instr.substr(12, 5));
+        funct3 = std::bitset<3>(instr.substr(17, 3));
+        std::bitset<5> imm0 = std::bitset<5>(instr.substr(20, 5));
+        opcode = std::bitset<7>(instr.substr(25, 7));
+
+        imm = std::bitset<12>(imm1.to_string() + imm0.to_string());
+    }
+
+    void execute() override {}
+};
+
+// Branch operations
+struct BType : Instruction 
+{
+    std::bitset<7> opcode;
+    std::bitset<3> funct3;
+    std::bitset<5> rs1;
+    std::bitset<5> rs2;
+    // This value represents a multiple of 2, meaning 
+    // it must be shifted left by 1 to get the actual value
+    // This will be done in the execute method
+    std::bitset<12> imm; 
+
+    BType(std::bitset<32> instruction)
+    {
+        std::string instr = instruction.to_string();
+
+        std::bitset<1> imm3 = std::bitset<1>(instr.substr(0, 1));
+        std::bitset<6> imm1 = std::bitset<6>(instr.substr(1, 6));
+        rs2 = std::bitset<5>(instr.substr(7, 5));
+        rs1 = std::bitset<5>(instr.substr(12, 5));
+        funct3 = std::bitset<3>(instr.substr(17, 3));
+        std::bitset<4> imm0 = std::bitset<4>(instr.substr(20, 4));
+        std::bitset<1> imm2 = std::bitset<1>(instr.substr(24, 1));
+        opcode = std::bitset<7>(instr.substr(25, 7));
+
+        imm = std::bitset<12>(imm3.to_string() + imm2.to_string() + imm1.to_string() + imm0.to_string());
+    }
+
+    void execute() override {}
+};
+
+// Upper immediate operations
+struct UType : Instruction 
+{
+    std::bitset<7> opcode;
+    std::bitset<5> rd;
+    std::bitset<20> imm;
+
+    UType(std::bitset<32> instruction)
+    {
+        std::string instr = instruction.to_string();
+
+        imm = std::bitset<20>(instr.substr(0, 20));
+        rd = std::bitset<5>(instr.substr(20, 5));
+        opcode = std::bitset<7>(instr.substr(25, 7));
+    }
+
+    void execute() override {}
+};
+
+// Jump operations
+struct JType : Instruction 
+{
+    std::bitset<7> opcode;
+    std::bitset<5> rd;
+    // This value represents a multiple of 2, meaning 
+    // it must be shifted left by 1 to get the actual value
+    // This will be done in the execute method
+    std::bitset<20> imm; 
+
+    JType(std::bitset<32> instruction)
+    {
+        std::string instr = instruction.to_string();
+
+        std::bitset<1> imm3 = std::bitset<1>(instr.substr(0, 1));
+        std::bitset<10> imm0 = std::bitset<10>(instr.substr(1, 10));
+        std::bitset<1> imm1 = std::bitset<1>(instr.substr(11, 1));
+        std::bitset<8> imm2 = std::bitset<8>(instr.substr(12, 8));
+        rd = std::bitset<5>(instr.substr(20, 5));
+        opcode = std::bitset<7>(instr.substr(25, 7));
+
+        imm = std::bitset<20>(imm3.to_string() + imm2.to_string() + imm1.to_string() + imm0.to_string());
+    }
+
+    void execute() override {}
+};
+
+///
 
 struct OperandInstruction : Instruction
 {
